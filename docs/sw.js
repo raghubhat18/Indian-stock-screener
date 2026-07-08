368d8f47b1eed42d688f5413f8e0e4684f023671
raghubhat18 <raghubhat18@gmail.com>
2026-07-08 19:24:25 +0530

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,3 +1,24 @@
-self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
+const CACHE_NAME = 'niftypulse-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install event - caches the basic files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch event - serves from cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
